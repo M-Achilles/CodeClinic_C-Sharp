74fc405af63e54a97f8ea3178fcff92bd8ba3f49
M-Achilles <39596516+M-Achilles@users.noreply.github.com>
2019-12-04 07:50:06 +0100

    WhereAmI: Request of geolocation data from device API
    Use of NuGet-Package Xamarin.Essentials to request geolocation data. Simple presenting of data as text.

--- WhereAmI/WhereAmI/PositionPage.xaml.cs
+++ WhereAmI/WhereAmI/PositionPage.xaml.cs
@@ -10,12 +10,41 @@ using Xamarin.Forms.Xaml;
 
 namespace WhereAmI
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PositionPage : ContentPage
-	{
-		public PositionPage ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PositionPage : ContentPage
+    {
+        public PositionPage ()
+        {
+            InitializeComponent ();
+
+                Location location = Task.Run(async ()=> await GetDeviceLocationAsync()).Result;
+                if (location != null)
+                {
+                    latitudeValue.Text = location.Latitude.ToString();
+                    logitudeValue.Text = location.Longitude.ToString();
+                }
+                else
+                {
+                    latitudeValue.Text = "0.0";
+                    logitudeValue.Text = "0.0";
+                }
+
+        }
+
+        private async Task<Location> GetDeviceLocationAsync()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                var location = await Geolocation.GetLocationAsync(request);
+
+                return location;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured: " + ex.ToString());
+                return null;
+            }
+        }
 	}
 }
