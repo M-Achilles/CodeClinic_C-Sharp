432907aa37d5e813e45ce6a31c3f5b3562126e3a
M-Achilles <marcoachilles@gmx.de>
2020-04-05 21:16:35 +0200

    Add new project Eight_Queens

---
+++ Eight_Queens/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eight_Queens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
